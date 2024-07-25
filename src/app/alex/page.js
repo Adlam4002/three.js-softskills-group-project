4bb1f8a9f15572fe3d245ef91170815627c1529c
Adlam4002 <adlam4002@hotmail.co.uk>
2024-07-25 13:44:20 +0100

    Update page.js

--- src/app/alex/page.js
+++ src/app/alex/page.js
@@ -1,12 +1,19 @@
 "use client";
 import css from "@/styles/alexHome.module.css";
-import OrbitControls from "@/components/OrbitControls";
-// import Draggable from "@/components/Draggable";
-import Sun from "@/components/Sun";
-import Earth from "@/components/Earth";
-import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { Suspense, useEffect, useState } from "react";
 import { TextureLoader } from "three";
 import { useLoader } from "@react-three/fiber";
+import Link from "next/link";
+
+// Dynamically import the Canvas component
+const Canvas = dynamic(() => import("@react-three/fiber").then(mod => mod.Canvas), {
+  ssr: false
+});
+
+import OrbitControls from "@/components/OrbitControls";
+import Sun from "@/components/Sun";
+import Earth from "@/components/Earth";
 import Mercury from "@/components/Mercury";
 import Venus from "@/components/Venus";
 import Mars from "@/components/Mars";
@@ -14,18 +21,19 @@ import Jupiter from "@/components/Jupiter";
 import Saturn from "@/components/Saturn";
 import Uranus from "@/components/Uranus";
 import Neptune from "@/components/Neptune";
-import { RGBELoader } from "../utils/RGBELoader";
-import * as THREE from "three";
-import { extend, createRoot, events } from "@react-three/fiber";
-import { Environment, useEnvironment } from "@react-three/drei";
 import Scene from "@/components/Scene";
 import LightBulb from "@/components/LightBulb";
-import Link from "next/link";
-import dynamic from "next/dynamic";
-import { Canvas } from "@react-three/fiber"
-// import Flares from "@/components/Flares";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Set the flag to true if the window object is defined
+    if (typeof window !== "undefined") {
+      setIsClient(true);
+    }
+  }, []);
+
   const earthTexture = useLoader(TextureLoader, "/2k_earth_daymap.jpg");
   const mercuryTexture = useLoader(TextureLoader, "/2k_mercury.jpg");
   const sunTexture = useLoader(TextureLoader, "/2k_sun.jpg");
@@ -36,37 +44,34 @@ export default function Home() {
   const uranusTexture = useLoader(TextureLoader, "/2k_uranus.jpg");
   const neptuneTexture = useLoader(TextureLoader, "/2k_neptune.jpg");
 
-  // const starsTexture = useLoader(TextureLoader, "/stars.jpg");
-
   return (
     <div className={css.scene}>
       <Link href={"/"} className="fixed z-10 bg-opacity-0 m-5">
         Home
       </Link>
-      <Canvas
-        shadows
-        className={css.canvas}
-        camera={{
-          position: [-6, 7, 7],
-        }}
-      >
-        <Scene />
-        {/* <Draggable> */}
-        <Suspense fallback={null}>
-          {/* <LightBulb /> */}
-          <Mercury map={mercuryTexture} />
-          <Venus map={venusTexture} />
-          <Earth map={earthTexture} />
-          <Mars map={marsTexture} />
-          <Jupiter map={jupiterTexture} />
-          <Saturn map={saturnTexture} />
-          <Uranus map={uranusTexture} />
-          <Neptune map={neptuneTexture} />
-          <Sun map={sunTexture} />
-        </Suspense>
-        {/* </Draggable> */}
-        <ambientLight color={"white"} intensity={0.5} />
-      </Canvas>
+      {isClient && (
+        <Canvas
+          shadows
+          className={css.canvas}
+          camera={{
+            position: [-6, 7, 7],
+          }}
+        >
+          <Scene />
+          <Suspense fallback={null}>
+            <Mercury map={mercuryTexture} />
+            <Venus map={venusTexture} />
+            <Earth map={earthTexture} />
+            <Mars map={marsTexture} />
+            <Jupiter map={jupiterTexture} />
+            <Saturn map={saturnTexture} />
+            <Uranus map={uranusTexture} />
+            <Neptune map={neptuneTexture} />
+            <Sun map={sunTexture} />
+          </Suspense>
+          <ambientLight color={"white"} intensity={0.5} />
+        </Canvas>
+      )}
     </div>
   );
 }
